250f2230e681253487206e4563fea07c39e07777
eugene2050 <eugenioeugene09@gmail.com>
2023-07-13 15:59:11 +0800

    ye

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Order;
 use App\Models\OrderItems;
 use App\Models\Customer;
-use Carbon\Carbon;
+
 
 class ReportController extends Controller
 {
@@ -25,14 +25,14 @@ class ReportController extends Controller
                 return [
                     'product' => $orderItem->product->name,
                     'quantity' => $orderItem->quantity,
-                    'price' => number_format($orderItem->price, 2), // format the price to 2 decimals
+                    'price' => number_format($orderItem->price, 2),
                 ];
             });
     
             return [
                 'customer' => $customerName,
                 'order_items' => $orderItems,
-                'total' => number_format($order->total(), 2), // format the total to 2 decimals
+                'total' => number_format($order->total(), 2), 
             ];
         });
     
@@ -40,14 +40,14 @@ class ReportController extends Controller
         $pdf->AddPage();
         $pdf->SetFont('Arial','B',12);
     
-        // Table header
+  
         $pdf->Cell(60,10,'Customer',1);
         $pdf->Cell(60,10,'Product',1);
         $pdf->Cell(30,10,'Quantity',1);
         $pdf->Cell(30,10,'Price',1);
-        $pdf->Ln();  // New line
+        $pdf->Ln();  
     
-        // Table body
+       
         $totalPrice = 0;
         foreach($data as $item) {
             $pdf->SetFont('Arial','',12);
@@ -56,19 +56,18 @@ class ReportController extends Controller
                 $pdf->Cell(60,10,$orderItem['product'],1);
                 $pdf->Cell(30,10,$orderItem['quantity'],1);
                 $pdf->Cell(30,10,$orderItem['price'],1);
-                $pdf->Ln();  // New line
+                $pdf->Ln();  
     
-                // Calculate total price
                 $totalPrice += $orderItem['price'];
             }
         }
     
-        // Total row
+      
         $pdf->SetFont('Arial','B',12);
         $pdf->Cell(150,10,'Total',1);
         $pdf->Cell(30,10,$totalPrice,1);
-        $pdf->Ln();  // New line
-        $content = $pdf->Output('report.pdf', 'S');  // get content as a string
+        $pdf->Ln();  
+        $content = $pdf->Output('report.pdf', 'S');  
         
         return response($content)->header('Content-Type', 'application/pdf');
     }
