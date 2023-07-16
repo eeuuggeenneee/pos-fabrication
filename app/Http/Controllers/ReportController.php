ede5bb414a2c3f1719d8d575a863f0a05ef00450
eugene2050 <eugenioeugene09@gmail.com>
2023-07-16 19:01:57 +0800

    design

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -32,43 +32,46 @@ class ReportController extends Controller
             return [
                 'customer' => $customerName,
                 'order_items' => $orderItems,
-                'total' => number_format($order->total(), 2), 
+                'total' => number_format($order->total(), 2),
             ];
         });
     
         $pdf = new \FPDF();
         $pdf->AddPage();
-        $pdf->SetFont('Arial','B',12);
+        $pdf->SetFont('Arial', 'B', 30);
     
-  
-        $pdf->Cell(60,10,'Customer',1);
-        $pdf->Cell(60,10,'Product',1);
-        $pdf->Cell(30,10,'Quantity',1);
-        $pdf->Cell(30,10,'Price',1);
-        $pdf->Ln();  
+        $pdf->Cell(190, 10, 'Order Report', 0, 1, 'C');
+        $pdf->SetFont('Arial', 'B', 15);
+        $pdf->Cell(190, 10, 'Date Range: ' . $fromDate . ' to ' . $toDate, 0, 1, 'C');
+        $pdf->Ln();
     
-       
+        $pdf->SetFont('Arial', 'B', 12);
+        $pdf->Cell(60, 10, 'Customer', 1, 0, 'C');
+        $pdf->Cell(60, 10, 'Product', 1, 0, 'C');
+        $pdf->Cell(30, 10, 'Quantity', 1, 0, 'C');
+        $pdf->Cell(40, 10, 'Price', 1, 0, 'C');
+        $pdf->Ln();
+    
         $totalPrice = 0;
-        foreach($data as $item) {
-            $pdf->SetFont('Arial','',12);
-            foreach($item['order_items'] as $orderItem) {
-                $pdf->Cell(60,10,$item['customer'],1);
-                $pdf->Cell(60,10,$orderItem['product'],1);
-                $pdf->Cell(30,10,$orderItem['quantity'],1);
-                $pdf->Cell(30,10,$orderItem['price'],1);
-                $pdf->Ln();  
+        foreach ($data as $item) {
+            $pdf->SetFont('Arial', '', 12);
+            foreach ($item['order_items'] as $orderItem) {
+                $pdf->Cell(60, 10, $item['customer'], 1, 0, 'C');
+                $pdf->Cell(60, 10, $orderItem['product'], 1, 0, 'C');
+                $pdf->Cell(30, 10, $orderItem['quantity'], 1, 0, 'C');
+                $pdf->Cell(40, 10, $orderItem['price'], 1, 0, 'C');
+                $pdf->Ln();
     
                 $totalPrice += $orderItem['price'];
             }
         }
     
-      
-        $pdf->SetFont('Arial','B',12);
-        $pdf->Cell(150,10,'Total',1);
-        $pdf->Cell(30,10,$totalPrice,1);
-        $pdf->Ln();  
-        $content = $pdf->Output('report.pdf', 'S');  
-        
+        $pdf->SetFont('Arial', 'B', 12);
+        $pdf->Cell(150, 10, 'Total', 1, 0, 'C');
+        $pdf->Cell(40, 10, $totalPrice, 1, 0, 'C');
+        $pdf->Ln();
+        $content = $pdf->Output('report.pdf', 'S');
+    
         return response($content)->header('Content-Type', 'application/pdf');
     }
     
