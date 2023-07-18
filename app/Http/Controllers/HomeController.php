becf98d1b107aa0f6cbedfd81299c845eccc4380
eugene2050 <eugenioeugene09@gmail.com>
2023-07-18 17:26:36 +0800

    chart

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Order;
 use App\Models\Customer;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
+use Carbon\CarbonPeriod;
 
 class HomeController extends Controller
 {
@@ -26,23 +28,59 @@ class HomeController extends Controller
     public function index()
     {
         $orders = Order::with(['items', 'payments'])->get();
+        $startDate = Carbon::now()->startOfMonth()->setTimezone('Asia/Manila');
+        $endDate = Carbon::now()->endOfMonth()->setTimezone('Asia/Manila');
+
+        $incomeData = $orders->whereBetween('created_at', [$startDate, $endDate])
+            ->groupBy(function ($order) {
+                return $order->created_at->format('Y-m-d');
+            })->map(function ($group) {
+                return $group->sum(function ($order) {
+                    return min($order->receivedAmount(), $order->total());
+                });
+            });
+
+        $dates = [];
+        $currentDate = $startDate->copy();
+
+        while ($currentDate <= $endDate) {
+            $dates[] = $currentDate->format('Y-m-d');
+            $currentDate->addDay();
+        }
+
+        $filledIncomeData = collect($dates)->map(function ($date) use ($incomeData) {
+            return $incomeData->has($date) ? $incomeData->get($date) : 0;
+        });
+
+        $chartData = [
+            "labels" => $dates,
+            "datasets" => [
+                [
+                    "label" => "Income",
+                    "data" => $filledIncomeData->values()->toArray(),
+                    "backgroundColor" => "rgba(255, 237, 74, 255)",
+                    "borderColor" => "rgba(229,213,66,255)",
+                    "borderWidth" => 4,
+                ],
+            ],
+        ];
+
         $customers_count = Customer::count();
+
+        $totalIncome = $filledIncomeData->sum();
 
         return view('home', [
             'orders_count' => $orders->count(),
-            'income' => $orders->map(function($i) {
-                if($i->receivedAmount() > $i->total()) {
-                    return $i->total();
-                }
-                return $i->receivedAmount();
-            })->sum(),
-            'income_today' => $orders->where('created_at', '>=', date('Y-m-d').' 00:00:00')->map(function($i) {
-                if($i->receivedAmount() > $i->total()) {
-                    return $i->total();
-                }
-                return $i->receivedAmount();
-            })->sum(),
-            'customers_count' => $customers_count
+            'income' => $orders->sum(function ($order) {
+                return min($order->receivedAmount(), $order->total());
+            }),
+            'income_today' => $orders->where('created_at', '>=', now()->startOfDay())->sum(function ($order) {
+                return min($order->receivedAmount(), $order->total());
+            }),
+            'customers_count' => $customers_count,
+            'chartData' => $chartData,
+            'totalIncome' => $totalIncome,
+            'startDate' => $startDate,
         ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,64 +5,134 @@
 @section('content')
     <div class="container-fluid">
         <div class="row">
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-info">
-              <div class="inner">
-                  <h3>{{$orders_count}}</h3>
-                <p>Orders Count</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-bag"></i>
-              </div>
-              <a href="{{route('orders.index')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            <div class="col-lg-3 col-6">
+                <!-- small box -->
+                <div class="small-box bg-info">
+                    <div class="inner">
+                        <h3>{{ $orders_count }}</h3>
+                        <p>Orders Count</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-bag"></i>
+                    </div>
+                    <a href="{{ route('orders.index') }}" class="small-box-footer">More info <i
+                            class="fas fa-arrow-circle-right"></i></a>
+                </div>
             </div>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-success">
-              <div class="inner">
-                  <h3>{{config('settings.currency_symbol')}} {{number_format($income, 2)}}</h3>
-                <p>Income</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-stats-bars"></i>
-              </div>
-              <a href="{{route('orders.index')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            <!-- ./col -->
+            <div class="col-lg-3 col-6">
+                <!-- small box -->
+                <div class="small-box bg-success">
+                    <div class="inner">
+                        <h3>{{ config('settings.currency_symbol') }} {{ number_format($income, 2) }}</h3>
+                        <p>Income</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-stats-bars"></i>
+                    </div>
+                    <a href="{{ route('orders.index') }}" class="small-box-footer">More info <i
+                            class="fas fa-arrow-circle-right"></i></a>
+                </div>
             </div>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-danger">
-              <div class="inner">
-                <h3>{{config('settings.currency_symbol')}} {{number_format($income_today, 2)}}</h3>
+            <!-- ./col -->
+            <div class="col-lg-3 col-6">
+                <!-- small box -->
+                <div class="small-box bg-danger">
+                    <div class="inner">
+                        <h3>{{ config('settings.currency_symbol') }} {{ number_format($income_today, 2) }}</h3>
 
-                <p>Income Today</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-pie-graph"></i>
-              </div>
-              <a href="{{route('orders.index')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        <p>Income Today</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-pie-graph"></i>
+                    </div>
+                    <a href="{{ route('orders.index') }}" class="small-box-footer">More info <i
+                            class="fas fa-arrow-circle-right"></i></a>
+                </div>
             </div>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-warning">
-              <div class="inner">
-                <h3>{{$customers_count}}</h3>
+            <!-- ./col -->
+            <div class="col-lg-3 col-6">
+                <!-- small box -->
+                <div class="small-box bg-warning">
+                    <div class="inner">
+                        <h3>{{ $customers_count }}</h3>
 
-                <p>Customers Count</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-person-add"></i>
-              </div>
-              <a href="{{ route('customers.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        <p>Customers Count</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-person-add"></i>
+                    </div>
+                    <a href="{{ route('customers.index') }}" class="small-box-footer">More info <i
+                            class="fas fa-arrow-circle-right"></i></a>
+                </div>
             </div>
-          </div>
-          <!-- ./col -->
+            <!-- ./col -->
+
+
+
+        </div>
+        <div class="card" style="background-color: #1f1f1f">
+            <div class="card-header text-white" style="background-color: #332e2e">
+                <h3>30 Days Income Chart</h3>
+            </div>
+            <div class="text-white text-center mt-2">Total Income for {{ $startDate->format('F Y') }}: {{ config('settings.currency_symbol') }}
+                {{ number_format($totalIncome, 2) }}</div>
+            <div class="card-body">
+
+                <canvas id="line-chart" style="height: 600px;"></canvas>
+            </div>
+        </div>
+
+
     </div>
-</div>
+
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            const chartData = @json($chartData);
+
+            const ctx = document.getElementById('line-chart').getContext('2d');
+            new Chart(ctx, {
+                type: 'line',
+                data: chartData,
+                options: {
+                    plugins: {
+                        legend: {
+                            display: false
+                        },
+                        tooltip: {
+                            backgroundColor: '#343a40',
+                            bodyColor: '#ffffff'
+                        }
+                    },
+                    scales: {
+                        x: {
+                            grid: {
+                                color: '#ffffff'
+                            },
+                            ticks: {
+                                color: '#ffffff'
+                            }
+                        },
+                        y: {
+                            grid: {
+                                color: '#ffffff'
+                            },
+                            ticks: {
+                                color: '#ffffff'
+                            }
+                        }
+                    },
+                    elements: {
+                        line: {
+                            borderWidth: 10,
+                            borderColor: '#ffffff'
+                        }
+                    },
+                    responsive: true,
+                    maintainAspectRatio: false
+                }
+            });
+        });
+    </script>
 @endsection
